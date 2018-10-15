4f8000636b8bc5ec6fda5fb2190066aea3d4a992
Fantom-3000 <Fantom-3000@users.noreply.github.com>
2018-10-15 21:24:41 +0300

    v. 181015.2120

--- personal.py
+++ personal.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QTableWidgetItem
 import sys
 from functools import partial
 
-def read_users_data(sql):
+def read_data(sql):
     conn = lite.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(sql)
@@ -13,15 +13,18 @@ def read_users_data(sql):
     return users_data
 
 # Обработка нажатия кнопки "Отправить"
-def enter_button_clk(table, users_list_box, stations_list_box, persons_list_box, schedule_list_box):
+def enter_button_clk(table_login_model, users_list_box, stations_list_box, persons_list_box, schedule_list_box, logins):
     user = users_list_box.currentText()
+    # user = logins.data(QtGui.QStandardItem.text(1)) # Не работает
     station = stations_list_box.currentText()
     person = person_list_box.currentText()
     schedule = schedule_list_box.currentText()
     station_id = station_list.index(station) + 1 # Индекс станции
     person_id = person_list.index(person)
     schedule_id = schedule_list.index(schedule) * 2 + 1 # Индекс ячейки графика + в качестве n лица
-    table.setItem(schedule_id + person_id, station_id, QTableWidgetItem(user))
+    login = table_login_model.data(table_login_model.index(schedule_id + person_id, station_id))
+    if not login:
+        table_login_model.setItem(schedule_id + person_id, station_id, QtGui.QStandardItem(user))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
@@ -38,31 +41,32 @@ if __name__ == '__main__':
     h_box_1 = QtWidgets.QHBoxLayout()
     h_box_2 = QtWidgets.QHBoxLayout()
 
-    table = QtWidgets.QTableWidget()
-    table.setColumnCount(7)
-    table.setRowCount(9)
-    table.setFixedSize(750, 300)
-    table.horizontalHeader().hide()
-    table.verticalHeader().hide()
-
     users_list_box = QtWidgets.QComboBox()
     stations_list_box = QtWidgets.QComboBox()
     person_list_box = QtWidgets.QComboBox()
     schedule_list_box = QtWidgets.QComboBox()
     enter_button = QtWidgets.QPushButton('Отправить')
+    logins_list_box = QtWidgets.QListView()
+    logins_list_box.setFixedSize(150, 300)
 
-    users_login_list = QtWidgets.QListWidget()
-    users_login_list.setFixedSize(200, 300)
+    login_table_view = QtWidgets.QTableView()
+    table_login_model = QtGui.QStandardItemModel(9, 7)
+    login_table_view.setModel(table_login_model)
+    login_table_view.setFixedSize(750, 300)
+    login_table_view.horizontalHeader().hide()
+    login_table_view.verticalHeader().hide()
 
     # Звполнение строк названиями станций из базы данных
-    data = read_users_data(sql_stations)
+    data = read_data(sql_stations)
     station_list = []
     for station_data in data:
-        station_list.append(station_data[1])    
-    stations_list_box.addItems(station_list)
+        station_list.append(station_data[1])
+    stations = QtCore.QStringListModel(station_list)
+    stations_list_box.setModel(stations)
+    # stations_list_box.addItems(station_list)
 
     # Звполнение строк номерами графиков из базы данных
-    data = read_users_data(sql_schedule)
+    data = read_data(sql_schedule)
     schedule_list = []
     for schedule_data in data:
         schedule_list.append(schedule_data[1])
@@ -70,13 +74,17 @@ if __name__ == '__main__':
 
     # Заполнение строк фамилиями и инициалами из базы данных
     if users_list_box.currentIndexChanged != -1:
-        data = read_users_data(sql_users)
+        login_list = []
+        data = read_data(sql_users)
         for user_data in data:
-            users_list_box.addItem(user_data[1])
+            login_list.append(user_data[1])
+        logins = QtCore.QStringListModel(login_list)
+        logins_list_box.setModel(logins)
+        users_list_box.setModel(logins)
 
     # Звполнение строк в качестве лица из базы данных
     if person_list_box.currentIndexChanged != -1:
-        data = read_users_data(sql_persons)
+        data = read_data(sql_persons)
         person_list = []
         for person_data in data:
             person_list.append(person_data[1])
@@ -84,14 +92,15 @@ if __name__ == '__main__':
 
     # Внесение в таблицу наименований столбцов и строк
     for col in range(1, 9, 2): # Столбцы - наименования станций
-        table.setSpan(col, 0, 2, 1)
-        table.setItem(col, 0, QTableWidgetItem(schedule_list[(col)//2]))
+        login_table_view.setSpan(col, 0, 2, 1)
+        table_login_model.setItem(col, 0, QtGui.QStandardItem(schedule_list[(col)//2]))
 
     for row in range(1, 7): # Строки - наименования графиков
-        table.setItem(0, row, QTableWidgetItem(station_list[row-1]))    
+        table_login_model.setItem(0, row, QtGui.QStandardItem(station_list[row-1]))
 
-    h_box_1.addWidget(users_login_list, QtCore.Qt.AlignLeft)
-    h_box_1.addWidget(table, QtCore.Qt.AlignLeft)
+    h_box_1.addWidget(logins_list_box)
+    h_box_1.addWidget(login_table_view)
+
     h_box_2.addWidget(users_list_box)
     h_box_2.addWidget(stations_list_box)
     h_box_2.addWidget(schedule_list_box)
@@ -101,9 +110,9 @@ if __name__ == '__main__':
     box.addLayout(h_box_1)
     box.addLayout(h_box_2)
 
-    # Обработчик кнопки "Отправить"
-    enter_button.clicked.connect(partial(enter_button_clk, table, users_list_box, 
-        stations_list_box, person_list_box, schedule_list_box))
+    # Обработчик нажатия кнопки "Отправить"
+    enter_button.clicked.connect(partial(enter_button_clk, table_login_model, users_list_box, 
+        stations_list_box, person_list_box, schedule_list_box, logins))
 
     window.show()
     sys.exit(app.exec_())
